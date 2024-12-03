bba50ce8a86dbb7346f58c649fbd92e8f50f29a6
ppabis <piotr@pabis.eu>
2024-12-03 14:19:12 +0100

    A basic VPC and network setup

---
+++ infrastructure/.terraform.lock.hcl
@@ -0,0 +1,38 @@
+# This file is maintained automatically by "tofu init".
+# Manual edits may be lost in future updates.
+
+provider "registry.opentofu.org/hashicorp/aws" {
+  version     = "5.78.0"
+  constraints = "~> 5.0"
+  hashes = [
+    "h1:R/7gwC9DMlTTgGwKNfp59H4RuKiE87vN4YL1n4XSG6I=",
+    "zh:22f63d4f062b30ab6ca5f24d6217e948d6836415798a5cdb2819e6c89c9e17ae",
+    "zh:6f30507b9bf95a33be00ae8df72c37060f3fc37342d9eaf8647dccb7fc69cbce",
+    "zh:803239ae5e8b539d4845f4a79a119c58e9a584242432638596dfba5d1a92418c",
+    "zh:8520f6969b31b1a48c5ed42767ee7560b5b009d5d8eba7209d66383242d64ad9",
+    "zh:8f0c17c084f498fc91572672d533fb9e3cbcbd2ca6b082a1f86e5865fd7ff714",
+    "zh:9a2c59675d1d38bae227ed937895a7d0e41f797ea07f3c5645adec42bb7fb738",
+    "zh:b74410e55c54f288c84176e389eacfb1098aa4a738f761e1ef108c0f16623db4",
+    "zh:b7ac49d9c480cfe9531463a4cc197e76b8c4f8bb77a84088b15360c031b5ba05",
+    "zh:e7e9a5d7c27a1dd4971e9871588341864d200997d8b5159e0c4bfdbb41a4b339",
+    "zh:ec122979dcc9b0ba3e31b45bc8eaf7053e996d9379dc4e7c5698c336081f8ae7",
+  ]
+}
+
+provider "registry.opentofu.org/hashicorp/local" {
+  version     = "2.5.2"
+  constraints = "~> 2.0"
+  hashes = [
+    "h1:MBgBjJljfDl1i2JPcIoH4hW+2XLJ+D1l12iH/xd3uTo=",
+    "zh:25b95b76ceaa62b5c95f6de2fa6e6242edbf51e7fc6c057b7f7101aa4081f64f",
+    "zh:3c974fdf6b42ca6f93309cf50951f345bfc5726ec6013b8832bcd3be0eb3429e",
+    "zh:5de843bf6d903f5cca97ce1061e2e06b6441985c68d013eabd738a9e4b828278",
+    "zh:86beead37c7b4f149a54d2ae633c99ff92159c748acea93ff0f3603d6b4c9f4f",
+    "zh:8e52e81d3dc50c3f79305d257da7fde7af634fed65e6ab5b8e214166784a720e",
+    "zh:9882f444c087c69559873b2d72eec406a40ede21acb5ac334d6563bf3a2387df",
+    "zh:a4484193d110da4a06c7bffc44cc6b61d3b5e881cd51df2a83fdda1a36ea25d2",
+    "zh:a53342426d173e29d8ee3106cb68abecdf4be301a3f6589e4e8d42015befa7da",
+    "zh:d25ef2aef6a9004363fc6db80305d30673fc1f7dd0b980d41d863b12dacd382a",
+    "zh:fa2d522fb323e2121f65b79709fd596514b293d816a1d969af8f72d108888e4c",
+  ]
+}
---
+++ infrastructure/network.tf
@@ -0,0 +1,86 @@
+#############################################################################
+#                              VPC                                          #
+#############################################################################
+resource "aws_vpc" "main" {
+  tags = { Name = "ecs-soci-vpc" }
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "main" {
+  tags = { Name = "ecs-soci-igw" }
+  vpc_id = aws_vpc.main.id
+}
+
+###############################################################################
+#                          Public subnets                                     #
+###############################################################################
+resource "aws_subnet" "public_1" {
+  tags = { Name = "ecs-soci-public-${var.aws_region}a" }
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = cidrsubnet(var.vpc_cidr, 8, 1)
+  availability_zone = "${var.aws_region}a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "public_2" {
+  tags = { Name = "ecs-soci-public-${var.aws_region}b" }
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = cidrsubnet(var.vpc_cidr, 8, 2)
+  availability_zone = "${var.aws_region}b"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_route_table" "public" {
+  tags = { Name = "ecs-soci-public-rt" }
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+}
+
+resource "aws_route_table_association" "public_1" {
+  subnet_id      = aws_subnet.public_1.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_2" {
+  subnet_id      = aws_subnet.public_2.id
+  route_table_id = aws_route_table.public.id
+}
+
+################################################################################
+#                          Private subnets                                     #
+################################################################################
+resource "aws_subnet" "private_1" {
+  tags = { Name = "ecs-soci-private-${var.aws_region}a" }
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = cidrsubnet(var.vpc_cidr, 8, 3)
+  availability_zone = "${var.aws_region}a"
+}
+
+resource "aws_subnet" "private_2" {
+  tags = { Name = "ecs-soci-private-${var.aws_region}b" }
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = cidrsubnet(var.vpc_cidr, 8, 4)
+  availability_zone = "${var.aws_region}b"
+}
+
+resource "aws_route_table" "private" {
+  tags = { Name = "ecs-soci-private-rt" }
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table_association" "private_1" {
+  subnet_id      = aws_subnet.private_1.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "private_2" {
+  subnet_id      = aws_subnet.private_2.id
+  route_table_id = aws_route_table.private.id
+}
