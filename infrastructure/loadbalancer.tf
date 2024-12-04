73bba5bcf66b8715da2684376fc97509599e6921
ppabis <piotr@pabis.eu>
2024-12-04 15:20:53 +0100

    Lock down loadbalancer from the public

--- infrastructure/loadbalancer.tf
+++ infrastructure/loadbalancer.tf
@@ -18,7 +18,7 @@ resource "aws_security_group" "alb" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = var.loadbalancer_allow_cidrs
   }
 
   egress {
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -9,3 +9,9 @@ variable "vpc_cidr" {
   type        = string
   default     = "10.13.0.0/16"
 }
+
+variable "loadbalancer_allow_cidrs" {
+  description = "CIDR blocks for load balancer"
+  type        = list(string)
+  default     = ["0.0.0.0/0"]
+}
